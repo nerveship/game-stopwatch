bb37f82467dccbf932c4d51f8303e1f44808d1f4
Johnny <johnstuartgonsales@gmail.com>
2023-01-14 14:03:43 +0000

    added check for sqlite version

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Data.SQLite;
+
+string cs = "Data Source=:memory:";
+string stm = "SELECT SQLITE_VERSION()";
+
+using var con = new SQLiteConnection(cs);
+con.Open();
+
+using var cmd = new SQLiteCommand(stm, con);
+string version = cmd.ExecuteScalar().ToString();
+
+Console.WriteLine($"SQLite version: {version}");
