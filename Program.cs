58eae4a6ee58e85d7dc74b5ae19ceae653276bcf
Johnny <johnstuartgonsales@gmail.com>
2023-01-14 14:20:54 +0000

    added some basic starting logic

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System.Data.SQLite;
+﻿/*using System.Data.SQLite;
 
 string cs = "Data Source=:memory:";
 string stm = "SELECT SQLITE_VERSION()";
@@ -9,4 +9,49 @@ con.Open();
 using var cmd = new SQLiteCommand(stm, con);
 string version = cmd.ExecuteScalar().ToString();
 
-Console.WriteLine($"SQLite version: {version}");
+Console.WriteLine($"SQLite version: {version}");*/
+
+using System;
+using System.Data.SQLite;
+
+
+
+namespace landing
+{
+    public static class Landing
+    {
+        public static int low = 0;
+        public static int high = 3;
+
+        public static string readString(string prompt)
+        {
+            string result;
+            do
+            {
+                Console.Write(prompt);
+                result = Console.ReadLine();
+            }
+            while (result == "");
+            return result;
+        }
+
+        public static int readInt(string prompt)
+        {
+            int result;
+            do
+            {
+                string intString = readString(prompt);
+                result = int.Parse(intString);
+            }
+            while ((result < low || (result > high)));
+            return result; 
+        }
+        static void Main()
+        {
+            Console.WriteLine("Welcome! What would you like to do today?");
+            int usrChoice = readInt("1. Log new game\n" +
+                "2. Check currently playing\n" +
+                "3. Quit\n");
+        }
+    }
+}
